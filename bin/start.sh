2fbe10e1099ff9823bc8771b44cf06949db947fc
Mr Phil Plencner <philip_plencner@harvard.edu>
2020-04-20 18:50:20 -0400

    Trying to get composer working with mysql and blacklight. No dice.

--- bin/start.sh
+++ bin/start.sh
@@ -2,4 +2,5 @@
 
 rm .internal_test_app/tmp/pids/server.pid 2> /dev/null
 bundle install
-bundle exec rake blacklight:server["-p 3000 -b '0.0.0.0'"]
+bundle exec rake db:migrate
+rails s -b localhost

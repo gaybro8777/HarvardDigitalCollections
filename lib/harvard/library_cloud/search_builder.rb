a290b5fdae55ceffa3caa8613ac998d981a12cd7
Matt Christian <matt@ifactory.com>
2020-05-19 17:36:08 -0400

    HLDP-492: fix for searching items in list

--- lib/harvard/library_cloud/search_builder.rb
+++ lib/harvard/library_cloud/search_builder.rb
@@ -6,6 +6,7 @@ module Harvard::LibraryCloud
 
     self.default_processor_chain += [:add_search_field_to_query]
     self.default_processor_chain += [:add_range_field_to_query]
+	self.default_processor_chain += [:add_setspec_field_to_query]
 
     def add_search_field_to_query(solr_parameters)
       solr_parameters[:search_field] = blacklight_config.search_fields[blacklight_params[:search_field]].key if blacklight_config.search_fields[blacklight_params[:search_field]]
@@ -14,6 +15,12 @@ module Harvard::LibraryCloud
     def add_range_field_to_query(solr_parameters)
       if !@blacklight_params[:range].nil?
         solr_parameters[:range] = @blacklight_params[:range]
+      end
+    end
+	
+    def add_setspec_field_to_query(solr_parameters)
+	  if !@blacklight_params[:setSpec].nil?
+        solr_parameters[:setSpec] = @blacklight_params[:setSpec]
       end
     end
 

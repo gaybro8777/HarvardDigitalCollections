0cf402ce985708bd85dde53bfac963e8441823c7
Mr Phil Plencner <philip_plencner@harvard.edu>
2020-06-17 12:03:05 -0400

    Added action mail configuration for SMTP server.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,6 +31,8 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = { host: ENV["EMAIL_HOST"], port: ENV["EMAIL_PORT"], from: ENV["EMAIL_FROM"] }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
